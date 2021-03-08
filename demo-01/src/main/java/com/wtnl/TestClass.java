5262b63326afe45a612a00f81887a0bdee85b33c
wcgzgj <zhangjia_good@126.com>
2021-03-08 19:29:00 +0800

    项目初始化

---
+++ demo-01/src/main/java/com/wtnl/TestClass.java
@@ -0,0 +1,14 @@
+package com.wtnl;
+
+/**
+ * @ClassName TestClass
+ * @Description TODO
+ * @Author FARO_Z
+ * @Date 2021/3/8 下午7:28
+ * @Version 1.0
+ **/
+public class TestClass {
+    public static void main(String[] args) {
+        System.out.println("wish us good luck");
+    }
+}
